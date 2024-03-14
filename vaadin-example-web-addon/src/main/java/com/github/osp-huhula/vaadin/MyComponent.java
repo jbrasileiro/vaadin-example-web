938cd245dcdb016ad5a65f819a50818dd1136220
João Carlos Brasileiro <jcbrasileiro@hotmail.com>
2024-03-13 22:18:57 -0300

    mvn archetype:generate ^    -DarchetypeGroupId=com.vaadin ^    -DarchetypeArtifactId=vaadin-archetype-application-multimodule ^    -DarchetypeVersion=8.14.3 ^    -DgroupId=com.github.osp-huhula.vaadin ^    -DartifactId=vaadin-example-web ^    -Dversion=0.0.1

---
+++ vaadin-example-web-addon/src/main/java/com/github/osp-huhula/vaadin/MyComponent.java
@@ -0,0 +1,9 @@
+package com.github.osp-huhula.vaadin;
+
+import com.vaadin.ui.AbstractComponent;
+
+/**
+ * This is a dummy example component.
+ */
+public class MyComponent extends AbstractComponent {
+}
